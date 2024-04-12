52cef71b27c16e3edff9a23ce6b8f359c351264f
Trolljerk <rimas40414243444546@gmail.com>
2024-04-11 22:11:37 -0500

    Cambio de carpeta y creacion de login - FINAL

---
+++ main.js
@@ -0,0 +1,61 @@
+// Cargar datos desde el archivo JSON y guardarlos en localStorage
+fetch("sources/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    localStorage.setItem("cartasData", JSON.stringify(data.data));
+    renderTable();
+  })
+  .catch((error) => console.error("Error al cargar los datos:", error));
+
+const datosFormulario = document.getElementById("datosForm");
+const datosTabla = document.querySelector("#tablaCartas tbody");
+const botones = document.querySelectorAll(".contenedor-cartas button");
+
+datosFormulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const numeroCarta = this.elements["numCarta"].value;
+  const cartasData = JSON.parse(localStorage.getItem("cartasData"));
+  const cartaExistente = cartasData.find((card) => card.numero === numeroCarta);
+
+  if (cartaExistente) {
+    cartaExistente.cantidad++;
+  }
+
+  localStorage.setItem("cartasData", JSON.stringify(cartasData));
+  renderTable();
+});
+
+botones.forEach((button) => {
+  button.addEventListener("click", function () {
+    const numeroCarta = this.getAttribute("carta");
+    const cartasData = JSON.parse(localStorage.getItem("cartasData"));
+    const cartaExistente = cartasData.find(
+      (card) => card.numero === numeroCarta
+    );
+    if (cartaExistente) {
+      cartaExistente.cantidad++;
+      localStorage.setItem("cartasData", JSON.stringify(cartasData));
+      renderTable();
+    }
+  });
+});
+
+function renderTable() {
+  datosTabla.innerHTML = "";
+
+  const cartasData = JSON.parse(localStorage.getItem("cartasData")) || [];
+
+  // Ordenar de mayor a menor por la cantidad de cartas
+  cartasData.sort((a, b) => b.cantidad - a.cantidad);
+
+  cartasData.forEach((card) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${card.numero}</td>
+    <td>${card.carta}</td>
+    <td>${card.cantidad}</td>
+    `;
+    datosTabla.appendChild(row);
+  });
+}
